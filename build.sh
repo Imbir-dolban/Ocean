1e8a9aca1fdddecfd25e8a8d9d7be5c262dcb280
Imbir-dolban <timatechtok@gmail.com>
2026-06-19 19:18:45 +0300

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "=== Компиляция Ассемблера ==="
+nasm -f elf32 multiboot.asm -o multiboot.o
+
+echo "=== Компиляция Си ==="
+gcc -m32 -march=i386 -ffreestanding -nostdlib -nodefaultlibs -c kernel.c -o kernel.o
+
+echo "=== Линковка ==="
+ld -m elf_i386 -Ttext 0x100000 multiboot.o kernel.o -o kernel.bin
+
+echo "=== Сборка ISO ==="
+cp kernel.bin iso/boot/
+grub-mkrescue -o ocean.iso iso
+
+echo "=== Готово! ==="
