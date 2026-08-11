14469ba35d0aaf81430f875aa24a5f8cb0588c17
Imbir-dolban <timatechtok@gmail.com>
2026-08-11 03:08:52 +0300

    Translate comments and variable names in multiboot.asm
    Updated comments and variable names to Russian.

--- multiboot.asm
+++ multiboot.asm
@@ -2,9 +2,9 @@
 
 section .multiboot
 align 4
-    dd 0x1BADB002            ; Magic number
-    dd 0x00                  ; Flags
-    dd - (0x1BADB002 + 0x00) ; Checksum
+    dd 0x1BADB002            ; Числа
+    dd 0x00                  ; Флаги
+    dd - (0x1BADB002 + 0x00) ; КонтрСумма
 
 section .bss
 align 16
@@ -14,13 +14,14 @@ stack_top:
 
 section .text
 global _start
-extern _start_c
+extern kernel_main
 
 _start:
     cli                      ; Выключаем прерывания
-    mov esp, stack_top       ; Устанавливаем указатель стека на вершину!
+    mov esp, stack_top       ; Устанавливаем указатель стека
+    xor ebp, ebp             ; Очищаем base pointer
     
-    call _start_c            ; Передаем управление в Си
+    call kernel_main         ; Передаем управление в Си
 
 .loop:
     hlt                      ; Если Си каким-то образом завершился — спим
